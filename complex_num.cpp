28474123bc3e12ca022bcb2046171c458727d8b5
yashk2373 <116952120+yashk2373@users.noreply.github.com>
2022-10-29 23:18:35 +0530

    Create complex_num.cpp

---
+++ complex_num.cpp
@@ -0,0 +1,20 @@
+#include <iostream>     
+#include <complex>      
+using namespace std;
+
+int main() {    
+  // defines the complex number: (3.0 + 4.0i)
+  std::complex<double> mycomplex(3.0, 4.0);
+  
+  cout << "Real part: " << real(mycomplex) << endl;
+  cout << "Imaginary part: " << imag(mycomplex) << endl;
+  cout << "The absolute value of " << mycomplex << " is: " << abs(mycomplex) << endl;
+  cout << "The argument of " << mycomplex << " is: " << cout << arg(mycomplex) << endl;
+  return 0;
+}
+
+// Output :-
+// Real part: 3
+// Imaginary part: 4
+// The absolute value of (3,4) is: 5
+// The argument of (3,4) is: 0.927295
